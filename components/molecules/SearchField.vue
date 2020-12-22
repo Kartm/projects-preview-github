b710cae6ffbec179724fc55f0091460579412baf
Łukasz Blachnicki <lblachnicki@gmail.com>
2020-12-22 10:11:36 +0100

    Reorganize components directory

---
+++ components/molecules/SearchField.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="header">
+    <input
+      v-model="inputValue"
+      @input="handleInput"
+      @change="handleChange"
+      :list="datalistId"
+      v-bind="$attrs"
+    />
+
+    <datalist :id="datalistId">
+      <option
+        v-for="option in options"
+        :value="option.value"
+        :key="option.value"
+      >
+        {{ option.description }}
+      </option>
+    </datalist>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, namespace, Prop } from 'nuxt-property-decorator';
+import generateId from '@/utils/generate-id';
+
+export interface SearchFieldOption {
+  value: string;
+  title: string;
+  description: string;
+  metadata: any;
+}
+
+@Component
+export default class SearchField extends Vue {
+  @Prop({ type: Array, default: () => [] })
+  private readonly options!: SearchFieldOption[];
+
+  private inputValue = '';
+  private readonly datalistId = generateId();
+
+  private handleInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.$emit('onInput', input.value);
+  }
+
+  private handleChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    const option = this.options.find((option) => option.value === input.value);
+
+    if (option) {
+      this.$emit('onSelect', option);
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>

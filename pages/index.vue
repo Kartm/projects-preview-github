21213b2349d64f2b1dc25cf90912f7e681667f85
Łukasz Blachnicki <lblachnicki@gmail.com>
2020-12-22 10:14:20 +0100

    Add search login to search page

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,89 @@
 <template>
   <div class="container">
     <h2>Project list</h2>
-    <search-field />
+    <search-field
+      :options="options"
+      @onInput="handleInput"
+      @onSelect="handleSelect"
+      placeholder="Provide GitHub project name"
+    />
   </div>
 </template>
 
 <script lang="ts">
-import SearchField from '@/components/SearchField.vue';
 import { Vue, Component } from 'nuxt-property-decorator';
+import SearchField, {
+  SearchFieldOption,
+} from '@/components/molecules/SearchField.vue';
+import debounce from '@/utils/debounce';
+
+export interface Metadata {
+  id: string;
+}
 
 @Component({
+  inheritAttrs: true,
   components: {
     SearchField,
   },
 })
-export default class MainPage extends Vue {}
+export default class MainPage extends Vue {
+  private options: SearchFieldOption[] = [];
+  private debouncedFetchData = debounce(this.fetchData, 500);
+
+  private handleInput(phrase: string) {
+    this.debouncedFetchData(phrase);
+  }
+
+  private handleSelect(option: SearchFieldOption) {
+    const metadata = option.metadata as Metadata;
+
+    this.$router.push({
+      path: `/project/${metadata.id}`,
+    });
+  }
+
+  async fetchData(phrase: string) {
+    this.$api
+      .$post(
+        '/graphql',
+        JSON.stringify({
+          query: `
+        {
+          search(query: "${phrase}", type: REPOSITORY, first: 10) {
+            edges {
+              node {
+                ... on Repository {
+                  id
+                  name
+                  description
+                  url
+                }
+              }
+            }
+          }
+        }
+    `,
+        })
+      )
+      .then((res) => res.data)
+      .then((data) => {
+        this.options = [
+          ...data.search.edges.map(
+            (edge: any) =>
+              ({
+                value: edge.node.url,
+                title: edge.node.name,
+                description: edge.node.description,
+                metadata: {
+                  id: edge.node.id,
+                } as Metadata,
+              } as SearchFieldOption)
+          ),
+        ];
+      });
+  }
+}
 </script>
 <style lang="scss">
 </style>
